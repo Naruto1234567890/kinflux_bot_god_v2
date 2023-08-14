17c7b033fd89287444dae6b280c6be34063f672e
Lionel <86529862+Naruto1234567890@users.noreply.github.com>
2023-08-14 16:54:37 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['14242345899', 'kinflux bot', true],
-  ['254110377776', 'zamzack254', true], 
+  ['24105621021', 'TOGE INUMAKI', true],
+  ['17245423663', 'MAKIMA', true], 
   [''] 
 ] //Number of owners
 
-global.mods = ['254110077535','254110377776','254765777423'] 
-global.prems = ['254110077535', '254110377776', '2250545752448']
+global.mods = ['24105621021','24105621021','24105621021'] 
+global.prems = ['24105621021', '24105621021', '24105621021']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -47,10 +47,10 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'kinflux boₜ 🐉'
+global.botname = 'MAKIMA V2'
 global.premium = 'true'
-global.packname = 'toxic┃ᴮᴼᵀ' 
-global.author = 'zamzack254' 
+global.packname = 'MAKIMA V2' 
+global.author = 'TOGE INUMAKI' 
 global.igfg = '▢ Follow on Instagram\nhttps://chat.whatsapp.com/KggVmnH7ki6HoyD0025gDD\n' 
 global.dygp = 'https://chat.whatsapp.com/KggVmnH7ki6HoyD0025gDD'
 global.fgsc = 'https://github.com/diggilly/kinflux-bot' 
